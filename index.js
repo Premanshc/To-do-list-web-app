f156cee0b061b0e5ebff860faa5260b6bad8ce6e
Premansh Chanderiya <pp4326@srmist.edu.in>
2023-09-03 15:15:09 +0530

    3rd august update

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import jquery from 'jquery';
+import ejs from 'ejs';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine','ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+//routes
 app.get('/',(req,res)=>{
     res.render('index.ejs');
 })
 
+let task = [];
 
+app.post('/add',(req,res)=>{
+    task.push(req.body.todoitem);
+    res.render('index.ejs',{taskItem:task});
+});
 
 app.listen(port,()=>{
     console.log(`Server is live at port: ${port}`);
